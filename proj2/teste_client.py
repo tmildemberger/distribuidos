bef56b62b0e20f2c92dfbc3ac420d87201d283db
Thiago MM <thimil@alunos.utfpr.edu.br>
2023-09-28 15:20:04 -0300

    teste melhor

--- proj2/teste_client.py
+++ proj2/teste_client.py
@@ -1,11 +1,26 @@
 # saved as greeting-client.py
 import Pyro5.api
+import threading
+import time
 
 @Pyro5.api.expose
 class Cli(object):
+    def __init__(self):
+        self.notified = False
+        
     @Pyro5.api.callback
     def callback(self, n):
+        self.notified = True
         print(n)
+
+    def requests(self, server):
+        while True:
+            server.req_callback(self, 5)
+            while not self.notified:
+                pass
+            self.notified = False
+            print("notified")
+            time.sleep(2)
 
 
 daemon = Pyro5.server.Daemon()         # make a Pyro daemon
@@ -14,8 +29,11 @@ client_uri = daemon.register(client)   # register the greeting maker as a Pyro o
 
 
 server = Pyro5.api.Proxy("PYRONAME:example.server")    # use name server object lookup uri shortcut
-server.req_callback(client, 25)
-server.req_callback(client, 10)
+#server.req_callback(client, 25)
+#server.req_callback(client, 10)
 
 print("ok")
-daemon.requestLoop()
+#daemon.requestLoop()
+th = threading.Thread(target=daemon.requestLoop)
+th.start()
+client.requests(server)
